0207e6c35527a4e3fd0caaf8d11c588a3e9eba85
Laksen Nayanajith <laksennayanajith21@gmail.com>
2025-04-05 01:20:34 +0530

    first commit

---
+++ src/main/java/com/taskmanager/api/entity/Task.java
@@ -0,0 +1,20 @@
+package com.taskmanager.api.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private String status;
+    private LocalDateTime createdAt;
+}
